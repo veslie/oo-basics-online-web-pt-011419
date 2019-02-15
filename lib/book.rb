173f2399e8705a061ba23fcb13d69e26495154c4
veslie losius <veslielosius@yahoo.com>
2019-02-15 09:13:07 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,40 @@
+class Book
+ 
+  def initialize(title)
+    @title = title
+  end
+ 
+  def title
+    @title
+  end
+ 
+   author=(author)
+    @author = author
+  end
+ 
+  def author
+    @author
+  end
+ 
+   page_count=(num)
+    @page_count = num
+  end
+ 
+  def page_count
+    @page_count
+  end
+ 
+   genre=(genre)
+    @genre = genre
+  end
+ 
+  def genre
+    @genre
+  end
+ 
+  def turn_page
+  end
+ 
+end
 
 
